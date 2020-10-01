c872c82c6d56b4e64d1de5198602281d4d6905f6
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-10-01 18:24:55 -0230

    Mod03-01-09

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var title = document.getElementById('title');
-
 var myButton = document.getElementById("btn");
+var counter = 0
+
 myButton.addEventListener('click', () => {
-  myButton.style.background = "red";
-  title.textContent = "I'm learning JS!!!";
+  // add one line
+  counter++;
+
+  // don't change this
+  myButton.textContent = counter;
 });
