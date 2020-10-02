11d6f4c3b4991f9b0268de4ef0fb2a5bd69f7178
over40dev <ted@over40.dev>
2020-10-02 16:54:19 -0230

    Mod03-02-03-complex data types

---
+++ 02 (JS Intro)/03 (complex types)/index.js
@@ -0,0 +1,11 @@
+/////////////////////////////////
+// Complex Data Types - Arrays //
+/////////////////////////////////
+
+// Arrays - List of ordered items
+// 0 indexed
+
+            //0.   //1.   //2. 
+var pets = ["Mark", "Larry", "Joe"]
+console.log(`Friend who joined 2nd is ${pets[1]}!`);
+console.log(...pets);
