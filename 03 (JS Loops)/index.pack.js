13dc80a73c94e2be27a44f16db0cd4e8d815eb6c
over40dev <ted@over40.dev>
2020-10-08 15:16:02 -0230

    Mod03-03-05 - For Loops with Arrays

--- 03 (JS Loops)/index.pack.js
+++ 03 (JS Loops)/index.pack.js
@@ -68,9 +68,24 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/cogentx/cx/_over40dev/edu-sb-fe-dev/Mod03-Interactive Web/03 (JS Loops)/index.js'");
+"use strict";
+
+
+//For Loops with arrays Mini Challenge: Print out 20 items from your own a
+
+var nhlTeams = ['Hurricanes', 'Blue Jackets', 'Devils', 'Islanders', 'Rangers', 'Flyers', 'Penguins', 'Capitals', 'Bruins', 'Sabres', 'Red Wings', 'Panthers', 'Canadiens', 'Senators', 'Lightning', 'Maple Leafs', 'Blackhawks', 'Avalanche', 'Stars', 'Wild', 'Predators', 'Blues', 'Jets', 'Ducks', 'Coyotes', 'Flames', 'Oilers', 'Kings', 'Sharks', 'Canucks', 'Golden Knights'];
+
+console.warn('\nNHL Teams: (for-loop)');
+for (var i = 0; i < 20 && i < nhlTeams.length; i++) {
+  console.log(i + 1 + '-' + nhlTeams[i]);
+}
+
+console.warn('\nNHL Teams: (forEach)');
+nhlTeams.forEach(function (t, i) {
+  return console.log(i + 1 + '-' + t);
+});
 
 /***/ })
 /******/ ]);
