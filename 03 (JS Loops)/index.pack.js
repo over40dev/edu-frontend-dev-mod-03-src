bb02fa03d3bcdd9c3b67214bd996cfb6891484ff
over40dev <ted@over40.dev>
2020-10-08 16:33:34 -0230

    Mod03-03-07 - color game board

--- 03 (JS Loops)/index.pack.js
+++ 03 (JS Loops)/index.pack.js
@@ -68,37 +68,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-// For Loops with if statements We are trying to find a table for my speed
-// dating group to sit at, that is the most economical for the restaurant. How
-// many options do I have?
-
-var tableNumbers = [5, 14, 7, 10, 20, 11, 12, 15, 3];
-
-for (var i = 0; i < tableNumbers.length; i++) {
-    if (tableNumbers[i] % 2 === 0) {
-        console.log(tableNumbers[i]);
-    }
-}
-
-// Mini Challenge: We have decided to play a game between all of us. Create a
-// loop that loops over the numbers given to each one of us. If the person has
-// even number, print out the word 'blue team'. If it is odd, print out the word
-// 'red team'.
-
-var people = 56;
-
-for (var _i = 1; _i <= people; _i++) {
-    if (_i % 2 === 0) {
-        console.log(_i + " - Blue Team");
-    } else {
-        console.log(_i + " - Red Team");
-    }
-}
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/cogentx/cx/_over40dev/edu-sb-fe-dev/Mod03-Interactive Web/03 (JS Loops)/index.js'");
 
 /***/ })
 /******/ ]);
---
+++ 03 (JS Loops)/index07.js
@@ -0,0 +1,19 @@
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+//Challenge: We use what we have learnt in the previous three lessons in order 
+//to visually colour a game board in our browser. At the moment the board is empty. 
+//Please colour the squares, similar to like you would on a chess board using the 
+//for loop.
+
+//Don't forget to share your code in the #share-your-code channel on discord.
+
+
+for (let i = 0; i < squares.length; i++) {
+    if (i % 2 === 0) {
+        squares[i].classList.add('even')
+    } else { 
+        squares[i].classList.add('odd') 
+    }
+}
+
+
